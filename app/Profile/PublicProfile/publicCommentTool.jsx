d5b9744ba2ed6655506584853a386adc5a8d4c15
Rob Cornell <rob.cornell@gmail.com>
2017-06-28 19:26:54 -0400

    Add button styling

--- app/Profile/PublicProfile/publicCommentTool.jsx
+++ app/Profile/PublicProfile/publicCommentTool.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
 const CommentTool = props => {
-  const SubmitButton = props.isEditing ? <button onClick={props.submitEdit}>Submit</button> : null;
+  const SubmitButton = props.isEditing 
+    ? <button
+        className="btn btn-small"
+        onClick={props.submitEdit}>
+        Submit
+      </button>
+    : null;
   return (
     <span>
-      <button onClick={props.openEditor}>Edit</button>
-      <button onClick={props.deleteComment}>Delete</button>
+      <button
+        className="btn btn-small"
+        onClick={props.openEditor}>
+        Edit
+      </button>
+      <button
+        className="btn btn-small"
+        onClick={props.deleteComment}>
+        Delete
+      </button>
       {SubmitButton}
     </span>
   );
