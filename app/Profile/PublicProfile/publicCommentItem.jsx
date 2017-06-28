24f4add4db5c3eaf3334bd33ba9d06ee1bff5fb6
Rob Cornell <rob.cornell@gmail.com>
2017-06-28 16:41:42 -0400

    Add methods and conditional rendering for editing mode

--- app/Profile/PublicProfile/publicCommentItem.jsx
+++ app/Profile/PublicProfile/publicCommentItem.jsx
@@ -1,18 +1,55 @@
 import React from 'react';
 import CommentTool from './publicCommentTool.jsx'
 
-const CommentItem = props => {
+class CommentItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      newComment: ''
+    };
+    this.editComment = this.editComment.bind(this);
+    this.openEditor = this.openEditor.bind(this);
+  }
+  handleUpdateComment(e) {
+    this.setState({newComment: e.target.value});
+  }
+  openEditor(e) {
+    this.setState({isEditing: true});
+  }
+  submitEdit(e) {
+    fetch('/api/comments', {
+      method: 'UPDATE',
+      body: this.state.newComment
+    })
+      .then(() => console.log('UPDATE SENT'))
+      .catch(err => console.log('Error updating comment'));
+  }
 
-  const CommentTool = props.currentUserId === props.comment.sender_id 
-    ? <CommentTool /> 
-    : null; 
-  return (
-    <div className="commentItem">
-      <div className="commentSubmitter">By {props.comment.sender.fullName} on {props.comment.createdAt.slice(0,10)}</div>
-      <p className="commentMessage">{props.comment.message}</p>
-      {CommentTool}
-    </div>
-  );
+  render() {
+      const CommentBody = this.state.isEditing 
+        ? (<input
+            type="text"
+            onChange={this.handleUpdateComment}
+            value={props.comment.message}
+          />)
+        : (<p
+            className="commentMessage">
+            {props.comment.message}
+          </p>);
+
+      const CommentTool = props.currentUserId === props.comment.sender_id 
+        ? <CommentTool isEditing={this.state.isEditing} /> 
+        : null; 
+
+    return (
+      <div className="commentItem">
+        <div className="commentSubmitter">By {props.comment.sender.fullName} on {props.comment.createdAt.slice(0,10)}</div>
+        {CommentBody}
+        {CommentTool}
+      </div>
+    );  
+  }  
 };
 
 export default CommentItem;
--- app/Profile/PublicProfile/publicCommentTool.jsx
+++ app/Profile/PublicProfile/publicCommentTool.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const CommentTool = props => {
-
+  const SubmitButton = props.isEditing ? <button onClick={props.submitEdit}>Submit</button> : null;
   return (
     <span>
-      <button onClick={props.editComment}>Edit</button>
+      <button onClick={props.openEditor}>Edit</button>
       <button onClick={props.deleteComment}>Delete</button>
+      {SubmitButton>}
     </span>
   );
 }
