b8a41f29437f867c03424b566f3e7691e6b93053
Amy Sun <amysun@Amys-MacBook-Air.local>
2017-07-01 13:48:41 -0400

    Add files that we need

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+        "jsx": true,
+        },
+    }
+  ]
+};
